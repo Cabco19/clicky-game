d4a58315986498228ac53c5ecdc7148e73138624
Ceara Owre <cearaowre@hotmail.com>
2020-02-06 23:55:44 -0500

    New Components added

---
+++ clicky-game/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+    return (
+      <nav className="navbar fixed-top navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">
+          Clicky Game
+        </a>
+      </nav>
+    );
+  }
+
+export default Nav;
