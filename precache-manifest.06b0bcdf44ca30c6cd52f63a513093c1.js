745e08969dd4f39dae5c947dfe4609f87ff689c9
Ceara Owre <cearaowre@hotmail.com>
2020-02-07 00:19:59 -0500

    Updates

---
+++ precache-manifest.06b0bcdf44ca30c6cd52f63a513093c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072b238731ea1a920c2a0b93d33b92ea",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ace95a54787771ee918f",
+    "url": "/clicky-game/static/css/main.7b2cdc40.chunk.css"
+  },
+  {
+    "revision": "646a75c58be7259d17ba",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace95a54787771ee918f",
+    "url": "/clicky-game/static/js/main.87dd0ad0.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
